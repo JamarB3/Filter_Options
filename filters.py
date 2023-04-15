9cfd383e3c41b25ebf7259eaed2fdb1e744a33c5
JamarB3 <102191390+JamarB3@users.noreply.github.com>
2023-04-15 13:28:05 -0400

    Update filters.py

--- filters.py
+++ filters.py
@@ -1,2 +1,17 @@
-print("hello world!")
-print(str(3 + 3))
+from PIL import Image
+
+# Open the image file
+image = Image.open("Superfly Elite 2")
+
+# Resize the image to fit in the terminal
+max_width, max_height = 100, 100
+image.thumbnail((max_width, max_height))
+
+# Convert the image to ASCII characters
+ascii_chars = [' ', '.', ':', '-', '=', '+', '*', '#', '%', '@']
+pixels = list(image.getdata())
+ascii_pixels = [ascii_chars[int(sum(pixel) / 3 / 25.5)] for pixel in pixels]
+ascii_image = ''.join(ascii_pixels)
+
+# Print the ASCII image in the terminal
+print(ascii_image)

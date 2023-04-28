ad0051a9598af694b01a245602185f152319d9b6
JamarB3 <102191390+JamarB3@users.noreply.github.com>
2023-04-27 23:45:09 -0400

    Added filters and matplot needed for package/functions

--- filters.py
+++ filters.py
@@ -1,17 +1,56 @@
-from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
 
-# Open the image file
-image = Image.open("Superfly Elite 2")
 
-# Resize the image to fit in the terminal
-max_width, max_height = 100, 100
-image.thumbnail((max_width, max_height))
+#For red stripes, r value = 255
 
-# Convert the image to ASCII characters
-ascii_chars = [' ', '.', ':', '-', '=', '+', '*', '#', '%', '@']
-pixels = list(image.getdata())
-ascii_pixels = [ascii_chars[int(sum(pixel) / 3 / 25.5)] for pixel in pixels]
-ascii_image = ''.join(ascii_pixels)
+  
+def red_stripes(image_matrix):
+    for row in range(len(image_matrix)):
+      if (row // 50) % 2 == 0:
+        for col in range(len(image_matrix[row])):
+          image_matrix[row][col][0] = 1.0
+          #1.0 = 255
+                
+    return image_matrix
+            
 
-# Print the ASCII image in the terminal
-print(ascii_image)
+            
+    
+    
+    
+    
+#Grayscale value: All RGB must be the same.
+
+def fade_to_black(image_matrix):
+    height, width, channels = image_matrix.shape
+    
+    for row in range(height):
+      for col in range(width):
+        for ch in range(channels):
+          image_matrix[row][col][ch] *= (col / width)
+                
+    return image_matrix
+
+
+def hyper_wave(image_matrix):
+  height, width, channels = image_matrix.shape
+  for row in range(height):
+    for col in range(width):
+      for ch in range(channels):
+        image_matrix[row][col][ch] **= (col / width)
+                
+  return image_matrix
+  
+
+
+# Read the image file
+img = mpimg.imread("valstrax.png")
+
+# Apply the red_stripes filter
+filtered_img = hyper_wave(img)
+
+# Display the filtered image using Matplotlib
+plt.imshow(filtered_img)
+plt.show()

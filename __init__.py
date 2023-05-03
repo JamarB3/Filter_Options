255ebc0b6ccb6181f3d5c1ea0bddc594572833e2
JamarB3 <102191390+JamarB3@users.noreply.github.com>
2023-05-03 15:49:48 -0400

    Created licensing and init files

---
+++ __init__.py
@@ -0,0 +1,21 @@
+# Change the content according to your package.
+# from directory.filename.py import classname
+from filters import red_stripes
+from filters import fade_to_black 
+from filters import hyper_wave
+from filters import red_distortion 
+
+__author__ = 'Miles Adedjouma, Jamar Bailey III, Darnell Love'
+__email__ = 'N/A'
+__version__ = '0.1.0'
+
+# module level doc-string
+__doc__ = """
+Filter Options
+================
+
+Description
+-----------
+The project was meant to be a test of the package functions in Github. In order to so, we created a project that was a photo filter function package using Python. The goal was to allow the user to take the package in order to add it to a greater function or to modify the function to allow users to add their own filters to the image. Furthermore, the function would allow users to input their own images in the functions. 
+
+"""
